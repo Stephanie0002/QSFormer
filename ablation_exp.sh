a1397ba8ae20392e4f4253695a40d3b223486c2c
Stephanie0002 <846967614@qq.com>
2024-07-26 16:13:33 +0000

    modified ablation_exp.sh

--- ablation_exp.sh
+++ ablation_exp.sh
@@ -1,5 +1,5 @@
 #Basic QSFormer
-# python ../FastDygForm/train_link_prediction.py --dataset_name uci --gpu 0 --order gradient-0.08-3 --model_name QSFormer --patch_size 4 --max_input_sequence_length 128 --num_runs 1 --num_hops 2 --dropout 0.15 --num_hops 2 --num_high_order_neighbors 3  > ../FastDygForm/gg2-gpu0-log.txt 2>&1 &
+# python train_link_prediction.py --dataset_name uci --gpu 0 --order gradient-0.08-3 --model_name QSFormer --patch_size 4 --max_input_sequence_length 128 --num_runs 1 --num_hops 2 --dropout 0.15 --num_hops 2 --num_high_order_neighbors 3  > gg2-gpu0-log.txt 2>&1 &
 
 gpu=$1
 data=$2
@@ -11,7 +11,7 @@ declare -a methods_list=("QSFormer")
 # for data in "${data_list[@]}"; do
 for method in "${methods_list[@]}"; do
     # 去除自适应小批次生成
-    cmd="python ../FastDygForm/train_link_prediction.py --dataset_name $data --gpu $gpu --order chorno --model_name $method --num_runs 1 --num_hops 2 --num_layers 2 --load_best_configs --ablation --test_interval_epochs 120"
+    cmd="python train_link_prediction.py --dataset_name $data --gpu $gpu --order chorno --model_name $method --num_runs 1 --num_hops 2 --num_layers 2 --load_best_configs --ablation --test_interval_epochs 120"
     echo "去除自适应小批次生成"
     echo "$cmd"
     eval "$cmd"
@@ -20,7 +20,7 @@ for method in "${methods_list[@]}"; do
     eval "$cmd2"
 
     # 去除二跳邻居
-    cmd="python ../FastDygForm/train_link_prediction.py --dataset_name $data --gpu $gpu --order gradient-0.08-3 --model_name $method --num_runs 1 --num_hops 1 --num_layers 2 --load_best_configs --ablation --test_interval_epochs 120"
+    cmd="python train_link_prediction.py --dataset_name $data --gpu $gpu --order gradient-0.08-3 --model_name $method --num_runs 1 --num_hops 1 --num_layers 2 --load_best_configs --ablation --test_interval_epochs 120"
     echo "去除二跳邻居"
     echo "$cmd"
     eval "$cmd"
@@ -29,7 +29,7 @@ for method in "${methods_list[@]}"; do
     eval "$cmd2"
 
     # 去除身份编码
-    cmd="python ../FastDygForm/train_link_prediction.py --dataset_name $data --gpu $gpu --order gradient-0.08-3 --model_name $method --num_runs 1 --num_hops 2 --num_layers 2 --no_id_encode --load_best_configs --ablation --test_interval_epochs 120"
+    cmd="python train_link_prediction.py --dataset_name $data --gpu $gpu --order gradient-0.08-3 --model_name $method --num_runs 1 --num_hops 2 --num_layers 2 --no_id_encode --load_best_configs --ablation --test_interval_epochs 120"
     echo "去除身份编码"
     echo "$cmd"
     eval "$cmd"
